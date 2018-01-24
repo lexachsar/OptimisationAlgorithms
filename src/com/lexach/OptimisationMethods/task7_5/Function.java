644392289823351a2e4c2e6fe99d7a19a0f2f6e1
lexach <lexachsar@gmail.com>
2018-01-24 14:18:51 +0400

    Создал структуру для метода градиентного спуска

--- src/com/lexach/OptimisationMethods/task7_5/Function.java
+++ src/com/lexach/OptimisationMethods/task7_5/Function.java
@@ -1,7 +1,29 @@
 package com.lexach.OptimisationMethods.task7_5;
 
 public class Function {
+    double[][] A = {4 -2 3}
+    double[] B =
+    double C
+
     public double getFunction(double x_1, double x_2) {
         return ( 45 * x_1 * x_1 - 88 * x_1 * x_2 + 45 * x_2 * x_2 + 102 * x_1 + 268 * x_2 - 21 );
+    }
+
+    public double[][] getA() {
+        return this.A;
+    }
+
+    public double[] getB() {
+        return this.B;
+    }
+
+    public double getC() {
+        return this.C;b        double[][] result = new double[][] {}
+
+        return double;
+    }
+
+    public double getGradient(double x) {
+        return ( this.A * x + this.B );
     }
 }
--- src/com/lexach/OptimisationMethods/task7_5/MethodOfGradientDescent.java
+++ src/com/lexach/OptimisationMethods/task7_5/MethodOfGradientDescent.java
@@ -1,7 +1,7 @@
 package com.lexach.OptimisationMethods.task7_5;
 
 public class MethodOfGradientDescent {
-    public static double methodOfGradientDescent() {
+    public static double methodOfGradientDescent(double epsilon, double alpha) {
 
     }
 
